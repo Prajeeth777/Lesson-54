f3521c56e9df45c325c6dc2208860527b94ef4b6
Prajeeth777 <prajeethkumar777@gmail.com>
2025-03-15 20:18:21 +0900

    Update Activity 1.py

--- Activity 1.py
+++ Activity 1.py
@@ -16,3 +16,50 @@ label1 = Label(root,
                bg='light blue')
 label1.place(relx=0.5, y=340, anchor=CENTER)
   
+def msg():
+    MsgBox = messagebox.showinfo(
+      "Alert", "Do you want to calculate the denomination count?"
+    if MsgBox == 'ok':
+      topwin()
+
+button1 = Button(root,
+                 text="Let's get started!",
+                 bg='brown',
+                 fg='white')
+button1.place(x=260, y=360)
+
+def topwin():
+  top = Toplevel()
+  top.title("Denominations Calculator")
+  top.configure(bg='light grey')
+  top.geometry('600x350+50+50')
+
+  label = Label(top, text="Enter total amount", bg='light grey')
+  entry = Entry(top)
+  lbl = Label(top, text="Here are number of notes for each denomination", bg='light grey')
+
+  l1= Label(top, text="2000", bg='light grey')
+  l1= Label(top, text="500", bg='light grey')
+  l1= Label(top, text="100", bg='light grey')
+
+  t1= Entry(top)
+  t2= Entry(top)
+  t3= Entry(top)
+
+  def calculator():
+    try:
+      global amount
+      amount = int(entry.get())
+      note2000= amount // 2000
+      amount %= 2000
+      note2000= amount // 500
+      amount %= 500
+      note2000= amount // 100
+      amount %= 100
+
+      t1.delete(0, END)
+      t2.delete(0, END)
+      t3.delete(0, END)
+except ValueError:
+    messagebox.showerror("Error", "Please enter a valid number.")
+    btn = Button
